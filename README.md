156c77360cb1b60aeb58c7df2d7ef9108674c677
deanrobin333 <deanrobin777@gmail.com>
2024-01-11 19:34:47 +0300

    updated clone_v2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <center> <h1>HBNB - The Console</h1> </center>
+<center> <h2>0x02. AirBnB clone - MySQL</h2> </center>
 
 This repository contains the initial stage of a student project to build a clone of the AirBnB website. This stage implements a backend interface, or console, to manage program data. Console commands allow the user to create, update, and destroy objects, as well as manage file storage. Using a system of JSON serialization/deserialization, storage is persistent between sessions.
 
@@ -8,7 +9,7 @@ This repository contains the initial stage of a student project to build a clone
 
 | Tasks | Files | Description |
 | ----- | ----- | ------ |
-| 0: Authors/README File | [AUTHORS](https://github.com/justinmajetich/AirBnB_clone/blob/dev/AUTHORS) | Project authors |
+| 0: Authors/README File | [AUTHORS](https://github.com/deanrobin333/AirBnB_clone_v2/blob/master/AUTHORS) | Project authors |
 | 1: Pep8 | N/A | All code is pep8 compliant|
 | 2: Unit Testing | [/tests](https://github.com/justinmajetich/AirBnB_clone/tree/dev/tests) | All class-defining modules are unittested |
 | 3. Make BaseModel | [/models/base_model.py](https://github.com/justinmajetich/AirBnB_clone/blob/dev/models/base_model.py) | Defines a parent class to be inherited by all model classes|
--- models/place.py
+++ models/place.py
@@ -1,18 +1,88 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from os import environ
+from sqlalchemy.orm import relationship
+import models
+from models.amenity import Amenity
+
+metadata = Base.metadata
+place_amenity = Table('place_amenity', metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id', ondelete='CASCADE')),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id', ondelete='CASCADE')))
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """
+    Define a place
+    """
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "places"
+
+        city_id = Column(String(60), ForeignKey("cities.id",
+                         ondelete='CASCADE'), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id",
+                         ondelete='CASCADE'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan',
+                               passive_deletes=True)
+        amenities = relationship('Amenity', backref='place_amenities',
+                                 cascade='all, delete',
+                                 secondary=place_amenity,
+                                 viewonly=False,
+                                 passive_deletes=True)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
+
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            from models.review import Review
+            return [review for review in models.storage.all(Review).values()
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place.
+            Returns:
+                list: list of amenity instances.
+            """
+            return [amenity for amenity in models.storage.all(Amenity).values()
+                    if amenity.place_id == self.id]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Handles append method for adding an Amenity.id to the attribute
+            amenity_ids. This method should accept only Amenity object,
+            otherwise, do nothing.
+            """
+            if not isinstance(obj, Amenity):
+                return
+            self.amenity_ids.append(obj.id)
