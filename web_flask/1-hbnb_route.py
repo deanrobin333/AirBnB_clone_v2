b6c028fb1100c44fcae4a5e2d42d2f22de30a39b
deanrobin333 <deanrobin777@gmail.com>
2024-07-28 19:23:53 +0000

    1-hbnb_route.py and modified 0-hell to listen on all hosts

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# 0-hello_route.py
+# 1-hbnb_route.py
 
 '''Script that starts a web application'''
 
-# im port flask class from flask module
+# import flask class from flask module
 from flask import Flask
 
 # create an instance called app of the class by passing __name__ variable
@@ -13,13 +13,21 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
-    """display "Hello HBNB!"
+    '''display "Hello HBNB!"
 
     Returns:
         str: text on the index page
-    """
+    '''
     return 'Hello HBNB!'
 
+@app.route('/hbnb')
+def hbnb():
+    '''displays "HBNB"
+
+    Returns:
+        str : text on the hbnb page
+    '''
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
