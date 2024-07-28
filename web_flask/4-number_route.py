75745314b301ed98e0cc6eef327fcee05b9557a0
deanrobin333 <deanrobin777@gmail.com>
2024-07-28 20:29:03 +0000

    4-number_route.py - right code, but display issue on browser

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# 4-number_route.py
+
+'''Script that starts a web application'''
+
+# import flask class from flask module
+from flask import Flask
+
+# create an instance called app of the class by passing __name__ variable
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''displays "HBNB"
+
+    Returns:
+        str : text on the hbnb page
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    """display "C", followed by the value of the text variable
+
+    Args:
+        text (str): text to be served on the page
+
+    Returns:
+        str: text on the page
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    '''display “Python ”, followed by the value of the text variable
+
+    Args:
+        text (str, optional) : text on page. Default is cool
+
+    Returns:
+        str: text on page
+    '''
+
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    '''display "{n} is a number"
+
+    Args:
+        n (int): number to be served on the page
+
+    Returns:
+        str: text on the page
+    '''
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
