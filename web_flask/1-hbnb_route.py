1fa92c4d6b0a3d04b6c5c1e91da382a592ad8a22
deanrobin333 <deanrobin777@gmail.com>
2024-07-28 19:25:35 +0000

    1-hbnb_route.py - pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -20,6 +20,7 @@ def index():
     '''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     '''displays "HBNB"
@@ -28,6 +29,7 @@ def hbnb():
         str : text on the hbnb page
     '''
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
